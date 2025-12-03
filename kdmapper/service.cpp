2ddc8f19f372ddbda0c9551fcb36416596335409
rekonstar <e80305427@gmail.com>
2025-12-03 14:24:13 +0100

    Add files via upload

---
+++ kdmapper/service.cpp
@@ -0,0 +1,110 @@
+#include "service.hpp"
+#include <Windows.h>
+#include <string>
+#include <iostream>
+
+#include "utils.hpp"
+#include "nt.hpp"
+
+bool service::RegisterAndStart(const std::wstring& driver_path, const std::wstring& serviceName) {
+	const static DWORD ServiceTypeKernel = 1;
+	const std::wstring servicesPath = L"SYSTEM\\CurrentControlSet\\Services\\" + serviceName;
+	const std::wstring nPath = L"\\??\\" + driver_path;
+
+	HKEY dservice;
+	LSTATUS status = RegCreateKeyW(HKEY_LOCAL_MACHINE, servicesPath.c_str(), &dservice); //Returns Ok if already exists
+	if (status != ERROR_SUCCESS) {
+		Log("[-] Can't create service key" << std::endl);
+		return false;
+	}
+
+	status = RegSetKeyValueW(dservice, NULL, L"ImagePath", REG_EXPAND_SZ, nPath.c_str(), (DWORD)(nPath.size()*sizeof(wchar_t)));
+	if (status != ERROR_SUCCESS) {
+		RegCloseKey(dservice);
+		Log("[-] Can't create 'ImagePath' registry value" << std::endl);
+		return false;
+	}
+	
+	status = RegSetKeyValueW(dservice, NULL, L"Type", REG_DWORD, &ServiceTypeKernel, sizeof(DWORD));
+	if (status != ERROR_SUCCESS) {
+		RegCloseKey(dservice);
+		Log("[-] Can't create 'Type' registry value" << std::endl);
+		return false;
+	}
+	
+	RegCloseKey(dservice);
+
+	HMODULE ntdll = GetModuleHandleA("ntdll.dll");
+	if (ntdll == NULL) {
+		return false;
+	}
+
+	//auto RtlAdjustPrivilege = (nt::RtlAdjustPrivilege)GetProcAddress(ntdll, "RtlAdjustPrivilege");
+	//auto NtLoadDriver = (nt::NtLoadDriver)GetProcAddress(ntdll, "NtLoadDriver");
+
+	ULONG SE_LOAD_DRIVER_PRIVILEGE = 10UL;
+	BOOLEAN SeLoadDriverWasEnabled;
+	NTSTATUS Status = nt::RtlAdjustPrivilege(SE_LOAD_DRIVER_PRIVILEGE, TRUE, FALSE, &SeLoadDriverWasEnabled);
+	if (!NT_SUCCESS(Status)) {
+		Log("Fatal error: failed to acquire SE_LOAD_DRIVER_PRIVILEGE. Make sure you are running as administrator." << std::endl);
+		return false;
+	}
+
+	std::wstring wdriver_reg_path = L"\\Registry\\Machine\\System\\CurrentControlSet\\Services\\" + serviceName;
+	UNICODE_STRING serviceStr;
+	RtlInitUnicodeString(&serviceStr, wdriver_reg_path.c_str());
+
+	Status = nt::NtLoadDriver(&serviceStr);
+
+
+	
+
+	if (Status == 0xC0000603) { //STATUS_IMAGE_CERT_REVOKED
+	}
+	else if (Status == 0xC0000022 || Status == 0xC000009A) { //STATUS_ACCESS_DENIED and STATUS_INSUFFICIENT_RESOURCES
+	
+	}
+	
+	
+	//Never should occur since kdmapper checks for "IsRunning" driver before
+	if (Status == 0xC000010E) {// STATUS_IMAGE_ALREADY_LOADED
+		return true;
+	}
+	
+	return NT_SUCCESS(Status);
+}
+
+bool service::StopAndRemove(const std::wstring& serviceName) {
+	HMODULE ntdll = GetModuleHandleA("ntdll.dll");
+	if (ntdll == NULL)
+		return false;
+
+	std::wstring wdriver_reg_path = L"\\Registry\\Machine\\System\\CurrentControlSet\\Services\\" + serviceName;
+	UNICODE_STRING serviceStr;
+	RtlInitUnicodeString(&serviceStr, wdriver_reg_path.c_str());
+
+	HKEY driver_service;
+	std::wstring servicesPath = L"SYSTEM\\CurrentControlSet\\Services\\" + serviceName;
+	LSTATUS status = RegOpenKeyW(HKEY_LOCAL_MACHINE, servicesPath.c_str(), &driver_service);
+	if (status != ERROR_SUCCESS) {
+		if (status == ERROR_FILE_NOT_FOUND) {
+			return true;
+		}
+		return false;
+	}
+	RegCloseKey(driver_service);
+
+	NTSTATUS st = nt::NtUnloadDriver(&serviceStr);
+	if (st != ERROR_SUCCESS) {
+		Log("[-] Decrypt Unload Failed [505]" << std::endl);
+		status = RegDeleteTreeW(HKEY_LOCAL_MACHINE, servicesPath.c_str());
+		return false; //lets consider unload fail as error because can cause problems with anti cheats later
+	}
+	
+
+	status = RegDeleteTreeW(HKEY_LOCAL_MACHINE, servicesPath.c_str());
+	if (status != ERROR_SUCCESS) {
+		return false;
+	}
+	return true;
+}
